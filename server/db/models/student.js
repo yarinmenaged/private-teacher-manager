8cbe271e1cea4873a35d90920c517fff4effa2fd
Barak Maron <Barak062@gmail.com>
2022-07-20 17:46:51 +0300

    fix: fixes naming of student model

--- server/db/models/student.js
+++ server/db/models/student.js
@@ -18,7 +18,7 @@ module.exports = (sequelize, DataTypes) => {
     Grade: DataTypes.NUMBER
   }, {
     sequelize,
-    modelName: 'Students',
+    modelName: 'Student',
   });
   return Students;
 };
