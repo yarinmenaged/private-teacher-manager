809f2e66b0bfc61b5085ab8de3a215064da74fa9
Barak Maron <Barak062@gmail.com>
2022-07-24 14:28:58 +0300

    refactor: delete mock events and get events from state

--- client/src/components/Schedule/CalendarContainer/HourBlock/HourBlockConnector.js
+++ client/src/components/Schedule/CalendarContainer/HourBlock/HourBlockConnector.js
@@ -1,47 +1,15 @@
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import HourBlock from './HourBlock';
+import { getEvents } from '../../../../redux/selectors/eventSelector';
 
 const mapStateToProps = (state, ownProps) => {
-    const events = [{
-        studentId: 1,
-        teacherId: 1,
-        date: "2022-07-23 07:00:00",
-        status: {},
-        subject: {
-            id: 5
-        }
-    },
-    {
-        studentId: 1,
-        teacherId: 3,
-        date: "2022-07-20 10:00:00",
-        status: {},
-        subject: {
-            id: 1
-        }
-    }, {
-        studentId: 1,
-        teacherId: 2,
-        date: "2022-07-17 07:00:00",
-        status: {},
-        subject: {
-            id: 2
-        }
-    }, {
-        studentId: 1,
-        teacherId: 2,
-        date: "2022-07-30 09:00:00",
-        status: {},
-        subject: {
-            id: 4
-        }
-    }];
+    const events = getEvents(state);
     return { ...ownProps, events };
 };
 
 const mapDispatchToProps = (dispatch, ownProps) => {
-    return bindActionCreators({ ...ownProps }, dispatch);
+    return bindActionCreators({  ...ownProps }, dispatch);
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(HourBlock);
