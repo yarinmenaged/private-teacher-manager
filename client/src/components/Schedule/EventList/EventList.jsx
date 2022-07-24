0a06f893c3e9274a1d317e0152347c84dca3d0a7
Stavush <stavbenyamin@gmail.com>
2022-07-24 10:50:32 +0300

    fix: Event a EventList components

--- client/src/components/Schedule/EventList/EventList.jsx
+++ client/src/components/Schedule/EventList/EventList.jsx
@@ -2,11 +2,9 @@ import React from "react";
 import style from "./EventList.module.css";
 import Event from "./Event/Event.jsx";
 
-function Event () {
-    let arr = ["8:00-8:45", "9:00-9:45"];
-    return(
-        {
-            arr.map((event) => <Event event={event}/>)
-        }
-    )
+function EventList() {
+  let arr = ["8:00-8:45", "9:00-9:45"];
+  return arr.map((event) => <Event event={event} />);
 }
+
+module.exports = EventList();
