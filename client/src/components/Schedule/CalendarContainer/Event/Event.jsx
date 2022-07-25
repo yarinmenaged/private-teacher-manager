0c09bdfa3b318bb01cd37aba045575c030bf0aa6
barakmaron <barak062@gmail.com>
2022-07-25 16:59:30 +0300

    feat: add blocked event to teacher

--- client/src/components/Schedule/CalendarContainer/Event/Event.jsx
+++ client/src/components/Schedule/CalendarContainer/Event/Event.jsx
@@ -5,13 +5,22 @@ import style from './Event.module.css';
 
 const Event = ({ event, user_type }) => {
   const time = moment(event.date).format(EventConstants.TimeFormat);
-  const name_string = user_type !== EventConstants.USER_TYPE.Teacher ? `Teacher:${event.Teacher.UserInfo.Name}` : `Student:${event.Student.UserInfo.Name}`;
-  return (
-    <div className={`${style.event} ${style[`color_${event.Subject.id}`]}`}>
-        <h3 className={style.title}>You has class with {name_string}</h3>
-        <p className={style.text}>{time}</p>
-  </div>
-  );
+  
+  if(event.Student){
+    const name_string = user_type !== EventConstants.USER_TYPE.Teacher ? `Teacher:${event.Teacher.UserInfo.Name}` : `Student:${event.Student.UserInfo.Name}`;
+    return (
+      <div className={`${style.event} ${style[`color_${event.Subject.id}`]}`}>
+          <h3 className={style.title}>You has class with {name_string}</h3>
+          <p className={style.text}>{time}</p>
+    </div>
+    );
+    }else
+    return (
+      <div className={`${style.event} ${style.blocked}`}>
+          <h3 className={style.title}>You blocked this hour</h3>
+          <p className={style.text}>{time}</p>
+    </div>
+    );
 }
 
 export default Event;
