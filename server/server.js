b5fe97aa85e1664bc4345eb354d9bb6876d80c11
Barak Maron <Barak062@gmail.com>
2022-07-25 12:30:36 +0300

    feat: server.js from deploy

--- server/server.js
+++ server/server.js
@@ -5,12 +5,18 @@ const envModule = require("./envModule");
 const app = express();
 const router = require("./routes/routes");
 
-sequelize.sync();
+sequelize.sync({ force: true });
 
 app.use(express.json());
 app.use(cors());
 app.use("/users", router);
 app.get("/login/google", passport.authenticate("google"));
+
+
+app.use('/static', express.static(path.join(__dirname, '../client/build/static')));
+app.get('*', function(req, res) {
+  res.sendFile('index.html', {root: path.join(__dirname, '../client/build/')});
+});
 
 app.listen(envModule.PORT, () =>
   console.log(`server is listening on port ${envModule.PORT}`)
