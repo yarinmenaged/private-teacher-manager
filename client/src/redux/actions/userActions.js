bb48c3292e1bd5f1230f81418c97f59c35afc63d
Aviranisa <aviranisa@gmail.com>
2022-08-05 12:42:33 +0300

    fix merge bug

--- client/src/redux/actions/userActions.js
+++ client/src/redux/actions/userActions.js
@@ -16,21 +16,21 @@ export const editAboutAction = (newAbout) => {
 };
 
 const editPrice = (newPrice) => ({
-  type: ACTIONS.EDIT_PRICE,
-  payload: newPrice,
+	type: ACTIONS.EDIT_PRICE,
+	payload: newPrice,
 });
 
 export const editPriceAction = (newPrice) => {
-  return async (dispatch, getState) => {
-    try {
-      const state = getState();
-      const id = state.userReducer.id;
-      dispatch(editPrice(newPrice));
-      await serverConnection.editPrice(id, newPrice);
-    } catch (error) {
-      dispatch(editPrice(0));
-    }
-  };
+	return async (dispatch, getState) => {
+		try {
+			const state = getState();
+			const id = state.userReducer.id;
+			dispatch(editPrice(newPrice));
+			await serverConnection.editPrice(id, newPrice);
+		} catch (error) {
+			dispatch(editPrice(0));
+		}
+	};
 };
 
 const getUserInfo = (userInfo) => ({
@@ -39,17 +39,10 @@ const getUserInfo = (userInfo) => ({
 });
 
 export const getUserInfoAction = (email, password) => {
-<<<<<<< HEAD
 	return async (dispatch) => {
 		const userInfo = await serverConnection.getUserInfo(email, password);
 		await dispatch(getUserInfo(userInfo));
 	};
-=======
-  return async (dispatch) => {
-    const userInfo = await serverConnection.getUserInfo(email, password);
-    await dispatch(getUserInfo(userInfo));
-  };
->>>>>>> 84eeb01b0d4ba5ce8bf3e6c538d7a58ee1eba605
 };
 
 const getUserInfoByToken = (userInfo) => ({
@@ -79,7 +72,6 @@ export const logOutAction = () => {
 };
 
 const addSubject = (newSubject) => ({
-<<<<<<< HEAD
 	type: ACTIONS.ADD_SUBJECT,
 	payload: newSubject,
 });
@@ -107,33 +99,4 @@ export const removeSubjectAction = (subject) => {
 		const removedSubject = { id: subject.id, Name: subject.value };
 		dispatch(removeSubject(removedSubject));
 	};
-=======
-  type: ACTIONS.ADD_SUBJECT,
-  payload: newSubject,
-});
-
-export const addSubjectAction = (subject) => {
-  return async (dispatch, getState) => {
-    const state = getState();
-    const userId = state.userReducer.id;
-    await serverConnection.addSubject(userId, subject.id);
-    const newSubject = { id: subject.id, Name: subject.value };
-    dispatch(addSubject(newSubject));
-  };
-};
-
-const removeSubject = (removedSubject) => ({
-  type: ACTIONS.REMOVE_SUBJECT,
-  payload: removedSubject,
-});
-
-export const removeSubjectAction = (subject) => {
-  return async (dispatch, getState) => {
-    const state = getState();
-    const userId = state.userReducer.id;
-    await serverConnection.removeSubject(userId, subject.id);
-    const removedSubject = { id: subject.id, Name: subject.value };
-    dispatch(removeSubject(removedSubject));
-  };
->>>>>>> 84eeb01b0d4ba5ce8bf3e6c538d7a58ee1eba605
 };
