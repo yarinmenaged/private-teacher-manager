2eec98c782f122f99206032d99df3597ba4ed8ec
Stavush <stavbenyamin@gmail.com>
2022-08-05 12:44:49 +0300

    style: changes in Home page style

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,23 +1,26 @@
-import style from './Home.module.css';
-import NavBar from '../NavBar/NavBarConnector';
-import { useEffect } from 'react';
-import EventConstants from '../Schedule/CalendarContainer/Event/Constants';
+import style from "./Home.module.css";
+import NavBar from "../NavBar/NavBarConnector";
+import { useEffect } from "react";
+import EventConstants from "../Schedule/CalendarContainer/Event/Constants";
 
+function Home({ Name, getAllSubjectsAction, user_type, GetSettingsAction }) {
+  useEffect(() => {
+    getAllSubjectsAction();
+    if (user_type === EventConstants.USER_TYPE.Teacher) GetSettingsAction();
+  }, []);
 
-function Home ({Name, getAllSubjectsAction, user_type, GetSettingsAction}) {
-
-    useEffect(() => {
-        getAllSubjectsAction();
-        if(user_type === EventConstants.USER_TYPE.Teacher)
-            GetSettingsAction();
-    }, []);
-
-    return (
-        <div>
-            <NavBar />
-            <h1 className={style.screenContainer}>Welcome {Name}</h1>
-        </div>
-    );
+  return (
+    <div>
+      <NavBar />
+      <div className={style.screenContainer}>
+        <h1>Welcome {Name}</h1>
+        <blockquote className={style.quote}>
+          "Education is the passport to the future, for tomorrow belongs to
+          those who prepare for it today." - Malcolm X
+        </blockquote>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
