99663480ebdb00cf09586145bb607e81f91a800b
Stavush <stavbenyamin@gmail.com>
2022-08-05 12:52:57 +0300

    merge dev into teacher_income

---
+++ client/src/components/Schedule/CalendarContainer/HourBlock/QuarterHourBlock/QuarterHourBlock.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './QuarterHourBlock.module.css';
+
+const QuarterHourBlock = () => {
+  return (
+    <div className={style.container}>
+      
+    </div>
+  )
+}
+
+export default QuarterHourBlock;
